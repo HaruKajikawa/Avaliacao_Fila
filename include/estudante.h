b460b153861a1f3ebc288c6e3bfc7faecd9ea9a2
HaruKajikawa <hotaruninho@gmail.com>
2025-12-05 14:16:04 -0400

    feat: adicionado o total e os dados dos estudantes em uma struct banco

--- include/estudante.h
+++ include/estudante.h
@@ -4,6 +4,14 @@
 #include <stdbool.h>
 #include "fila.h"
 
+#define MAX_CADASTRO 100
+
+typedef struct BancoEstudantes
+{
+    Aluno dados[MAX_CADASTRO];
+    int total;
+} BancoEstudantes;
+
 bool cadastrarEstudante(const Aluno* a);
 bool buscarMatricula(int matricula, Aluno* resultado);
 bool duplicidadeMatricula(int matricula);

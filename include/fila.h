5442210fc12bf21e6f1c8d0ff3cfb686aef1170a
HaruKajikawa <hotaruninho@gmail.com>
2025-12-05 14:49:43 -0400

    fix: melhoria

--- include/fila.h
+++ include/fila.h
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 
+#define avanca(idx, capacidade) (idx = ((idx + 1) % (capacidade)))
 #define MAX_NOME 61
 #define MAX_CPF 12
 
@@ -11,6 +12,7 @@ typedef struct Aluno
     int matricula;
     char cpf[MAX_CPF];
     char nome[MAX_NOME];
+    bool atendido;
 } Aluno;
 
 typedef struct Fila
@@ -25,10 +27,8 @@ typedef struct Fila
 Fila* criarFila(int capacidade);
 bool filaVazia(const Fila* F);
 bool filaCheia(const Fila* F);
-bool enfileirar(Fila* F, Aluno x);
-bool desinfileirar(Fila* F, Aluno* removido);
+bool enfileirar(Fila* F, const Aluno* x);
+bool desenfileirar(Fila* F, Aluno* removido);
 void destruirFila(Fila** F);
 void exibirFila(const Fila* F);
-bool existeMatriculaFila(const Fila* F, int matricula);
-bool existeCpfFila(const Fila* F, const char* cpf);
 #endif
